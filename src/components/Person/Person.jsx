10d23891193a2b61a3f99f7a9507f9bac5e65263
Tanya Baletska <t.baletska@gmail.com>
2024-08-04 10:15:35 +0300

    fixed code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,14 @@
 export default function Person({ person }) {
+  const personPartnerType = person.sex === 'm' ? 'wife' : 'husband';
+  const personPartner = person.isMarried
+    ? `${person.partnerName} is my ${personPartnerType}`
+    : 'I am not married';
+
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age ? <p className="Person__age">I am {person.age}</p> : null}
-      {person.isMarried ? (
-        <p className="Person__partner">
-          {person.partnerName} is my
-          {person.sex === 'f' ? ' husband' : ' wife'}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+      <p className="Person__partner">{personPartner}</p>
     </section>
   );
 }
